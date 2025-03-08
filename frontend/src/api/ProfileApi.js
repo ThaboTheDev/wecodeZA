439f7ef68f72d7b9bb25f953e3da8a7a11b7e12f
Alexander-Agu <theonlyagu@gmail.com>
2025-03-08 13:58:29 +0200

    Added the Get request to fetch post data

--- frontend/src/api/ProfileApi.js
+++ frontend/src/api/ProfileApi.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-import { useEffect } from "react";
 
 
+// Gets all users in the DB
+export const getUsers = async ()=>{
+    try{
+        const response = await axios.get("http://localhost:8080/api/wecode/users")
+        return response.data;
+    } catch (error){
+        console.log(error);
+        throw error
+    }
+}
+
+
+// Gets user information by ID
 export const getUserById = async (authorID) => {
     try {
         const response = await axios.get(`http://localhost:8080/api/wecode/users/${authorID}`);
