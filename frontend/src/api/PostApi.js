0e173992042afbae65ce29189df6966699eb8e96
Alexander-Agu <theonlyagu@gmail.com>
2025-03-08 14:19:26 +0200

    Added the Get request to fetch post data by PostId

--- frontend/src/api/PostApi.js
+++ frontend/src/api/PostApi.js
@@ -2,12 +2,24 @@ import axios from "axios";
 
 
 // Gets all post in the DB
-export const getPost = async ()=> {
+export const getPosts = async ()=> {
     try{
         const response =  await axios.get("http://localhost:8080/api/wecode/posts");
         return response.data;
     } catch (error){
         console.log(error);
         throw error;
+    }
+}
+
+
+// Get post data by user ID
+export const getPostById = async (id)=> {
+    try{
+        const response = await axios.get(`http://localhost:8080/api/wecode/posts/${id}`);
+        return response.data;
+    } catch (error){
+        console.log(error);
+        throw error
     }
 }
