540efab5a9358533ae9a4df78d22d0baa47d1a6c
Alexander-Agu <theonlyagu@gmail.com>
2025-03-09 11:03:00 +0200

    styled the login component and made it responsive

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -7,48 +7,45 @@ import { getLoginData } from '../../api/UserApi';
 function Login({getId}) {
     const [email, setEmail] = useState("");
     const [passcode, setPasscode] = useState("");
-    const [userId, setUserId] = useState();
-    const [page, setPage] = useState(0);
 
     const login = async ()=> {
         console.log("ghn")
         if (email, passcode){
-            getLoginData({
-                name: "alex",
-                email: "jclaudhjbh024@student.wethinkcode"
-            }) .then(()=>{
-                window.location.assign("/feed")
-                console.log(sessionStorage.getItem("userData"))
-            })
+            try{
+                getLoginData({
+                    password: passcode,
+                    email: email
+                }) .then(()=>{
+                    window.location.assign("/feed")
+                    console.log(sessionStorage.getItem("userData"))
+                })
+            } catch{
+                alert("Wrong login details, Try again!")
+            }
+        } else {
+            alert("Fill in all input boxes")
         }
     }
+    console.log(email)
+    return <>
+        <Header />
+        <article className='loginApp'>
+            <div className="loginContainer">
+                <div className="loginForm">
+                    <input type="text" onChange={e => setEmail(e.target.value)} placeholder='Email@gmail.com' value={email} />
+                    <input type="text" onChange={e => setPasscode(e.target.value)} placeholder='Password' value={passcode} />
 
-    if (page == 0){
-        return <>
-            <Header />
-            <article className='loginApp'>
-                <h2 className='loginWelcome'>
-                    Welcome back to WeCodeZa
-                </h2>
-
-                <div className="loginContainer">
-                    <div className="loginForm">
-                        <input type="text" onChange={e => setEmail(e.target.value)} placeholder='Email' value={email} />
-                        <input type="text" onChange={e => setPasscode(e.target.value)} placeholder='Password' value={passcode} />
-                    </div>
-
-                    <button className='loginSubmit' onClick={()=> login()}>
-                        Login
+                    <button onClick={()=> login()}>
+                        login
                     </button>
+
                     <p>
-                        Dont't have an account? <a>Sign Up</a> instead.
+                        Don't hav an account? <a href="/signup">SignUp</a> instead.
                     </p>
                 </div>
-            </article>
-        </>
-    } else if( page == 1 && userId){
-        
-    }
+            </div>
+        </article>
+    </>
 }
 
 export default Login;
