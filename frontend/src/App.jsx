c9f7d3ff8ed9c21dd3c73679113e19dd47525f25
Alexander-Agu <theonlyagu@gmail.com>
2025-03-09 10:30:38 +0200

    Updated my routing in the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,61 @@
-import React, { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Router, Routes } from 'react-router-dom'
-import Home from "./pages/home/Home";
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Feed from "./components/feed/Feed"
+import PostHome from './components/postHome/PostHome';
+import ViewProfile from './components/viewProfile/ViewProfile';
+import Profile from './components/profile/Profile';
+import Write from './components/write/Write';
+import Home from './pages/home/Home';
 import Login from './components/login/Login';
-import Signup from "./components/signup/Signup"
+import Signup from './components/signup/Signup';
+
+const router = createBrowserRouter([
+    {
+        path: "/feed",
+        element: <Feed />,
+        // errorElement: <h2>404 Page not found!</h2>
+    },
+    {
+        path: "/",
+        element: <Home />,
+        // errorElement: <h2>404 Page not found!</h2>
+    },
+    {
+        path: "/login",
+        element: <Login />,
+        // errorElement: <h2>404 Page not found!</h2>
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+        // errorElement: <h2>404 Page not found!</h2>
+    },
+    {
+        path: "/postHome/:postId",
+        element: <PostHome />
+    },
+    {
+        path: "/viewAuthor/:authorId",
+        element: <ViewProfile />
+    },
+    {
+        path: "/viewProfile",
+        element: <Profile />
+    },
+    {
+        path: "/write",
+        element: <Write />
+    },
+    {
+        path: "/editPost/:postId",
+        element: <Write />
+    }
+
+]);
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-        </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
