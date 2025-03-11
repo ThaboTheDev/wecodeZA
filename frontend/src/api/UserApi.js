1f8347a14a6d00a22e300f02cc1cf8a8896372c5
Alexander-Agu <theonlyagu@gmail.com>
2025-03-11 15:28:43 +0200

    Added the post request for creating a account

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -9,3 +9,18 @@ export const getLoginData = async (body)=> {
         throw error;
     }
 }
+
+
+export const createAccount = async (body)=> {
+    try{
+        const response = await axios.post("http://localhost:8080/api/wecode/users/signup", body, {
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
+        sessionStorage.setItem("userData", JSON.stringify(response.data));
+    } catch (error){
+        console.log(error);
+        throw error;
+    }
+}
